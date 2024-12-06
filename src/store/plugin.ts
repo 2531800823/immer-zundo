8e5835832dc8366a8bdb198cd28c089f212de15c
刘士朋 <2531800823@qq.com>
2024-12-07 00:45:59 +0800

    添加 ts 类型

--- src/store/plugin.ts
+++ src/store/plugin.ts
@@ -1,73 +1,120 @@
-import { applyPatches, produceWithPatches } from "immer";
-import { createStore } from "zustand";
+import { Patch, applyPatches, produceWithPatches } from "immer";
+import {
+  StateCreator,
+  StoreApi,
+  StoreMutatorIdentifier,
+  createStore,
+} from "zustand";
 import { immer } from "zustand/middleware/immer";
 
 const DEFAULT_LIMIT = 10;
 
-export const immerZundo = (initializer) => (set, get, store) => {
-  // 做一些插件处理
-  store.temporal = createStore(immer(temporalStateCreator(set, get)));
+interface State {
+  pastStates: Patch[][][];
+  futureStates: Patch[][][];
+}
 
-  const temporalHandleSet = (pastState) => {
-    const currentState = get();
-    store.temporal
-      .getState()
-      ._handleSet(pastState, undefined, currentState, undefined);
+interface TemporalState {
+  undo: (steps?: number) => void;
+  redo: (steps?: number) => void;
+  clear: () => void;
+
+  _handleSet: (pastState: Patch[][]) => void;
+}
+
+type Zundo = <
+  TState,
+  Mps extends [StoreMutatorIdentifier, unknown][] = [],
+  Mcs extends [StoreMutatorIdentifier, unknown][] = []
+>(
+  config: StateCreator<TState, [...Mps, ["temporal", unknown]], Mcs>,
+  options?: any
+) => StateCreator<
+  TState,
+  Mps,
+  [["temporal", StoreApi<TemporalState & State>], ...Mcs]
+>;
+
+export type Write<T, U> = Omit<T, keyof U> & U;
+declare module "zustand/vanilla" {
+  interface StoreMutators<S, A> {
+    temporal: Write<S, { temporal: A }>;
+  }
+}
+
+export const immerZundo = (<T>(initializer: StateCreator<T, [], []>) => {
+  const fn = (
+    set: StoreApi<T>["setState"],
+    get: StoreApi<T>["getState"],
+    store: StoreApi<T> & { temporal: StoreApi<TemporalState & State> }
+  ) => {
+    // 做一些插件处理
+    store.temporal = createStore<TemporalState & State>()(
+      immer((set1, get1) => {
+        const initialState: State = {
+          pastStates: [],
+          futureStates: [],
+        };
+        return {
+          ...initialState,
+          undo: () => {
+            if (get1().pastStates.length) {
+              set1((state) => {
+                const inversePatches = state.pastStates.pop()!;
+                const newState = applyPatches(
+                  get() as StoreApi<T>["getState"],
+                  inversePatches[1]
+                );
+                set(newState);
+                state.futureStates.push(inversePatches);
+              });
+            }
+          },
+          redo: () => {
+            if (get1().futureStates.length) {
+              set1((state) => {
+                const patches = state.futureStates.pop()!;
+                const newState = applyPatches(
+                  get() as StoreApi<T>["getState"],
+                  patches[0]
+                );
+                set(newState);
+                state.pastStates.push(patches);
+              });
+            }
+          },
+          clear: () => set1({ pastStates: [], futureStates: [] }),
+          _handleSet: (pastState) => {
+            set1((state) => {
+              if (state.pastStates.length >= DEFAULT_LIMIT) {
+                state.pastStates.shift();
+              }
+              state.futureStates = [];
+              state.pastStates.push(pastState);
+            });
+          },
+        };
+      })
+    );
+
+    // 更改 store.setState 这个是 react 中 useSyncExternalStore 返回的触发，重写 setState 函数
+    const setState = store.setState;
+    store.setState = (updater: any, replace: any, ...a) => {
+      const pastState = get();
+
+      const [nextState, patches, inversePatches] =
+        typeof updater === "function"
+          ? produceWithPatches(pastState, updater)
+          : updater;
+
+      setState(nextState, replace, ...a);
+
+      // 更新 设置缓存
+      store.temporal.getState()._handleSet([patches, inversePatches]);
+    };
+
+    return initializer(store.setState, get, store);
   };
 
-  // 更改 store.setState 这个是 react 中 useSyncExternalStore 返回的触发，重写 setState 函数
-  const setState = store.setState;
-  store.setState = (updater, replace, ...a) => {
-    const pastState = get();
-
-    const [nextState, patches, inversePatches] =
-      typeof updater === "function"
-        ? produceWithPatches(pastState, updater)
-        : updater;
-
-    setState(nextState, replace, ...a);
-
-    // 更新 设置缓存
-    temporalHandleSet([patches, inversePatches]);
-  };
-
-  return initializer(store.setState, get, store);
-};
-
-const temporalStateCreator = (userSet, userGet) => (set, get) => {
-  return {
-    pastStates: [],
-    futureStates: [],
-    undo: () => {
-      if (get().pastStates.length) {
-        set((state) => {
-          const inversePatches = state.pastStates.pop();
-          const newState = applyPatches(userGet(), inversePatches[1]);
-          userSet(newState);
-          state.futureStates.push(inversePatches);
-        });
-      }
-    },
-    redo: () => {
-      if (get().futureStates.length) {
-        set((state) => {
-          const patches = state.futureStates.pop()!;
-          const newState = applyPatches(userGet(), patches[0]);
-          userSet(newState);
-          state.pastStates.push(patches);
-        });
-      }
-    },
-    clear: () => set({ pastStates: [], futureStates: [] }),
-    _onSave: undefined,
-    _handleSet: (pastState) => {
-      set((state) => {
-        if (state.pastStates.length >= DEFAULT_LIMIT) {
-          state.pastStates.shift();
-        }
-        state.futureStates = [];
-        state.pastStates.push(pastState);
-      });
-    },
-  };
-};
+  return fn;
+}) as Zundo;
